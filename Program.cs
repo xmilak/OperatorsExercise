f6092247d9fe4e1651c783d5173d4191fe648dd2
Neven Milakovic <xxmilak@gmail.com>
2023-03-07 10:03:26 -0500

    finished project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var a = 17;
+            var b = 4;
+            var sum = a + b;
+            Console.WriteLine($"a + b = {sum}");
+
+            var diff = a - b;
+
+            Console.WriteLine($"a - b = {diff}");
+
+            Console.WriteLine($"a x b = {a * b}");
+
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            // Exercise 2
+
+            Console.WriteLine("What is radius of your circle?");
+
+            var radius = double.Parse( Console.ReadLine() );
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+            
+
+        }
+    }
+}
